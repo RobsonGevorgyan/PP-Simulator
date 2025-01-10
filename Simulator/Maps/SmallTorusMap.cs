915109e9affedbeecee876e2da93e7287f982a2e
RobsonGevorgyan <rgevorgyan42@gmail.com>
2025-01-10 20:17:37 +0100

    Testy

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
+{
+    public int Size { get; private set; }
+
+    public SmallTorusMap(int size)
+    {
+        throw new NotImplementedException();
+    }
+    public override bool Exist(Point p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Simulator/Validator.cs
+++ Simulator/Validator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Simulator;
 
-internal class Validator
+public class Validator
 
 {
     public static int Limiter(int value, int min, int max) 
